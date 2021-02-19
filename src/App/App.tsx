cc9b6bb4f1a27c3bfe0d4479aaa0e3de778c9fa5
frfn <flexer.fadrigalan@gmail.com>
2021-02-19 17:24:12 -0500

    created App folder for App files | the TS plugin CSS module is not working, it is ok

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,12 +1,12 @@
 import React from "react";
-import pizzas from "./data/pizzas.json";
-import Pizza from "./components/Pizza";
-import styles from "./App.module.css";
+import pizzas from "../data/pizzas.json";
+import Pizza from "../components/Pizza";
+import styles from "../App/App.module.css";
 
 const App = () => {
 	// (window as any).hello();  /* to simulate source mapping error for TS */
 
-	let content = <div>No Content</div>;
+	let content = <div className={styles["pizza-container"]}>No Content</div>;
 
 	if (pizzas) {
 		content = (
@@ -20,7 +20,7 @@ const App = () => {
 		);
 	}
 
-	return <div className={styles.pizza}>{content}</div>;
+	return <div className={styles["pizza-container"]}>{content}</div>;
 };
 
 export default App;
