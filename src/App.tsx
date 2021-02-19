b2b85dae0b38dc9ea5cff80a82a884d9ed27ee48
frfn <flexer.fadrigalan@gmail.com>
2021-02-19 11:13:57 -0500

    tweaked tsconfig to read JSON | added data/pizzas.json | added Pizza component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import pizzas from "./data/pizzas.json";
+import Pizza from "./components/Pizza";
 
 const App = () => {
-	return <div>App is outsourced!</div>;
+	// (window as any).hello();  /* to simulate source mapping error for TS */
+
+	let content = <div>No Content</div>;
+
+	if (pizzas) {
+		content = (
+			<ul>
+				{pizzas.map((pizza) => {
+					return <Pizza key={pizza.id} pizza={pizza} />;
+				})}
+			</ul>
+		);
+	}
+
+	return <div>{content}</div>;
 };
 
 export default App;
