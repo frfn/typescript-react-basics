733e111258890f460bcef4aa68fbcd9b97d4fd7c
frfn <flexer.fadrigalan@gmail.com>
2021-02-24 13:00:43 -0500

    we added useReducer! Check AppState.tsx

--- src/components/FoodCard/FoodCard.tsx
+++ src/components/FoodCard/FoodCard.tsx
@@ -18,73 +18,8 @@ interface Food {
 }
 
 const FoodCard: React.FC<Props> = ({ food }) => {
-	// context
-	const setState = useSetState(); // useSetState is an arrow function, must be executed | when it is, the value is NOW setState because of the provider
-
-	/* 
-	HERE:
-	imagine that you were to just call the useContext() here, 
-	- it fills the context with the setState function because of useContext()
-	- since the index.tsx component is wrapped with the AppStateProvider component, it gives the AppSetStateContext value
-	
-	it is the same concept when used in AppState.tsx
-	*/
-
 	// function to add to cart
-	const onAddToCart = () => {
-		// console.log(food); /* we see the description is on here as well, not compatible with the structure of the state used in AppState.tsx */
-
-		// Cannot invoke an object which is possibly 'undefined', that is why it is inside an `if` statement | not anymore, it a custom hook, it checks to see if setState is undefined!
-
-		setState((prevState) => {
-			// grouping logic
-			const itemExists = prevState.cart.items.find(
-				(item) => item.id === food.id
-			);
-
-			return {
-				...prevState,
-				cart: {
-					...prevState.cart,
-					items: itemExists
-						? prevState.cart.items.map((item) => {
-								// console.log(item.id, food.id); /* used to see why it was causing an error if I didn't `return item` at the end */
-								if (item.id === food.id) {
-									return {
-										...item,
-										quantity: item.quantity + 1,
-									};
-								}
-								// if it's not the item that we need to update, it still should return something
-								// this statement is inside a map(), so it must return something.
-
-								// since we're mapping, we are iterating through each element...
-								// so if we start iterating and it doesn't have the same ID as food.id, it should return that object untouched, and continue until it finds the right item!
-
-								/* 
-								if I add cheese pizza first, cheese pizza is first in the items array, 
-								Now, I add pepperoni, and I add another pepperoni again, map() will iterate through the items array, going through the cheese pizza first.
-
-								IT WILL GIVE ERROR, since food.id is not the same as the pizza.id
-
-								because it iterates through the whole entire array first, if it not the item we are looking for, just `return item`
-								*/
-								return item;
-						  })
-						: [
-								...prevState.cart.items,
-								// you can't do .push() because it is incompatible with the structure, in normal ReactJS, it is possible, but we're in TS
-								{
-									id: food.id,
-									name: food.name,
-									price: food.price,
-									quantity: 1,
-								},
-						  ],
-				},
-			};
-		});
-	};
+	const onAddToCart = () => {};
 
 	return (
 		<li className={styles["food-container"]}>
@@ -101,3 +36,73 @@ const FoodCard: React.FC<Props> = ({ food }) => {
 };
 
 export default FoodCard;
+
+/* 
+	HERE:
+	imagine that you were to just call the useContext() here, 
+	- it fills the context with the setState function because of useContext()
+	- since the index.tsx component is wrapped with the AppStateProvider component, it gives the AppSetStateContext value
+	
+	it is the same concept when used in AppState.tsx
+*/
+
+// context
+// const setState = useSetState(); // useSetState is an arrow function, must be executed | when it is, the value is NOW setState because of the provider
+
+/* 
+// function to add to cart
+const onAddToCart = () => {
+	// console.log(food);  we see the description is on here as well, not compatible with the structure of the state used in AppState.tsx
+
+	// Cannot invoke an object which is possibly 'undefined', that is why it is inside an `if` statement | not anymore, it a custom hook, it checks to see if setState is undefined!
+
+	setState((prevState) => {
+		// grouping logic
+		const itemExists = prevState.cart.items.find(
+			(item) => item.id === food.id
+		);
+
+		return {
+			...prevState,
+			cart: {
+				...prevState.cart,
+				items: itemExists
+					? prevState.cart.items.map((item) => {
+							// console.log(item.id, food.id); /* used to see why it was causing an error if I didn't `return item` at the end
+							if (item.id === food.id) {
+								return {
+									...item,
+									quantity: item.quantity + 1,
+								};
+							}
+							// if it's not the item that we need to update, it still should return something
+							// this statement is inside a map(), so it must return something.
+
+							// since we're mapping, we are iterating through each element...
+							// so if we start iterating and it doesn't have the same ID as food.id, it should return that object untouched, and continue until it finds the right item!
+
+							
+							if I add cheese pizza first, cheese pizza is first in the items array, 
+							Now, I add pepperoni, and I add another pepperoni again, map() will iterate through the items array, going through the cheese pizza first.
+
+							IT WILL GIVE ERROR, since food.id is not the same as the pizza.id
+
+							because it iterates through the whole entire array first, if it not the item we are looking for, just `return item`
+							
+							return item;
+					  })
+					: [
+							...prevState.cart.items,
+							// you can't do .push() because it is incompatible with the structure, in normal ReactJS, it is possible, but we're in TS
+							{
+								id: food.id,
+								name: food.name,
+								price: food.price,
+								quantity: 1,
+							},
+					  ],
+			},
+		};
+	});
+};
+*/
--- src/context/AppState.tsx
+++ src/context/AppState.tsx
@@ -1,9 +1,16 @@
-import React, { useState, createContext, useContext } from "react";
+import React, { useReducer, useState, createContext, useContext } from "react";
 
 /* -- Interface for Context -- */
+interface CartItem {
+	id: number;
+	name: string;
+	price: number;
+	quantity: number; // object []
+}
+
 interface IAppStateValue {
 	cart: {
-		items: { id: number; name: string; price: number; quantity: number }[]; // object []
+		items: CartItem[]; // object []
 	};
 }
 
@@ -19,49 +26,133 @@ interface Props {
 	children?: React.ReactNode; // copied from hovering over "props"
 }
 
-// 1. the cart context, giving it structure
+/* ---------------------------------------- */
+/* 1. the cart context, giving it structure */
+/* ---------------------------------------- */
 export const AppStateContext = createContext(DefaultAppState);
 
-// 1. the setCart, giving it the type is should receive
-export const AppSetStateContext = createContext<
-	React.Dispatch<React.SetStateAction<IAppStateValue>> | undefined
+/* ---------------------------------------------------- */
+/* 1. the setCart, giving it the type is should receive */
+/* ---------------------------------------------------- */
+export const AppDispatchContext /* AppSetStateContext */ = createContext<
+	| React.Dispatch<AddToCartAction>
+	/* React.Dispatch<React.SetStateAction<IAppStateValue>>, not anymore since useState is not used */
+	| undefined
 >(undefined);
 
-// 3. Custom Hook | outsourced so it is less bloated in FoodCard.tsx & to check if it is undefined!
-export const useSetState = () => {
+/* ------------------------------------------------------------------------------------------------ */
+/* 3. Custom Hook | outsourced so it is less bloated in action.payloadCitems.ard.tsx & to check if it is undefined! */
+/* ------------------------------------------------------------------------------------------------ */
+export const useStateDipatch /* useSetState */ = () => {
 	// the useContext, FILLS in the information from the createContext.
 	// at first it is undefined, but when given the value at the bottom, it WILL HAVE a value
 
 	// AppSetStateContext.Provider value={setState} gives the `value` of setState
-	// when using useSetState in FoodCard.tsx, it WILL be the setState function because the component is WRAPPED with the provider
+	// when using useSetState in action.payloadCitems.ard.tsx, it WILL be the setState function because the component is WRAPPED with the provider
 
 	// this here just gives it a structure, the provider down below gives it its value
 
 	// when useContext is called, useContext is going to retreive the value from the .Provider and FILL IN this structure, giving the createContext the setState function
-	const setState = useContext(AppSetStateContext);
-	if (!setState) {
+	const dispatch = useContext(AppDispatchContext);
+	if (!dispatch) {
 		throw new Error(
-			"useSetState was called outside of AppSetStateContext provider"
+			"useSetState was called outside of AppDispatchContext provider"
 		);
 	}
 
 	// and being able to return the function if it is not undefined.
-	return setState;
+	return dispatch;
 
 	// this will be FILLED in IF this custom is called in a component that is wrapped with the Provider component!
 };
 
-// 2. the component that will provide the context with data
+/* ------------ */
+/*  4. Reducer  */
+/* ------------ */
+// what's here --V
+interface Action<T> {
+	type: T; // <-- goes here
+}
+
+// since we pass ADD_TO_CART for the T argument, that must be the type
+// for AddToCartAction, to initialize it, it must contain type & payload
+/* 
+{
+	type: "ADD_TO_CART",
+}	payload: { items: { ... } }
+*/
+interface AddToCartAction extends Action<"ADD_TO_CART"> {
+	payload: {
+		item: CartItem;
+	};
+}
+export const reducer = (state: IAppStateValue, action: AddToCartAction) => {
+	switch (action.type) {
+		// if case "ADD_TO_CART", either create a new obj and put in array or update obj quantity
+		case "ADD_TO_CART":
+			// creating a variable to easily handle the object
+			const itemToAdd = action.payload.item;
+
+			// checking to see if items exist in the array
+			const itemExists = state.cart.items.find(
+				(item) => item.id === itemToAdd.id
+			);
+
+			// updating state immutably
+			return {
+				// this long hand approach deeply copies the object
+				...state,
+				cart: {
+					...state.cart,
+					// don't get confused, the ternary expression RETURNS an array which is expected
+					items: itemExists
+						? // MAP returns a new array
+						  state.cart.items.map((item) => {
+								// if the itemToAdd is in the array, just update the quantity and one
+								if (item.id === itemToAdd.id) {
+									return {
+										...item, // unpack and copy current props and values
+										quantity: item.quantity + 1,
+									};
+								}
+
+								// if not the item we're looking for, just return and move on to the next without touching the obj
+								return item;
+						  })
+						: // this is _already_ an array |
+						  [
+								...state.cart.items,
+								// you can't do .push() because it is incompatible with the structure, in normal ReactJS, it is possible, but we're in TS
+								{
+									...itemToAdd,
+									quantity: 1,
+								},
+						  ],
+				},
+			};
+
+		// just returns if no cases
+		default:
+			return state;
+	}
+};
+
+/* -------------------------------------------------------- */
+/* 2. the component that will provide the context with data */
+/* -------------------------------------------------------- */
 const AppStateProvider: React.FC<Props> = ({ children }) => {
 	// state is an object => {cart: {items: [] } }
-	const [state, setState] = useState(DefaultAppState);
+	const [state, dispatch /* setState */] = useReducer(
+		reducer,
+		DefaultAppState
+	); /* useState(DefaultAppState); */
 
 	return (
-		// cart is used in App.tsx | setCart is used in FoodCard.tsx
+		// cart is used in App.tsx | setCart is used in action.payloadCitems.ard.tsx
 		<AppStateContext.Provider value={state}>
-			<AppSetStateContext.Provider value={setState}>
+			<AppDispatchContext.Provider value={dispatch}>
 				{children}
-			</AppSetStateContext.Provider>
+			</AppDispatchContext.Provider>
 		</AppStateContext.Provider>
 	);
 };
